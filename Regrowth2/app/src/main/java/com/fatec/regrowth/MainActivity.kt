932d7c5996deeae043f8919dc8c418b5967b735b
Andrew Izaki <Andrewtsuyoshizaki@gmail.com>
2024-03-27 21:17:50 -0300

    atualização

--- Regrowth2/app/src/main/java/com/fatec/regrowth/MainActivity.kt
+++ Regrowth2/app/src/main/java/com/fatec/regrowth/MainActivity.kt
@@ -2,11 +2,26 @@ package com.fatec.regrowth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View;
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+
     }
+    fun cronometro(view: View){
+        val intent = Intent(this, cronometro::class.java)
+        startActivity(intent)
+    }
+    fun tree(view: View){
+        val intent = Intent(this, tree::class.java)
+        startActivity(intent)
+    }
+
+
 }
---
+++ Regrowth2/app/src/main/java/com/fatec/regrowth/cronometro.kt
@@ -0,0 +1,12 @@
+package com.fatec.regrowth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class cronometro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cronometro)
+    }
+}
